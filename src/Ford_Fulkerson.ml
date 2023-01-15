f12443ded453d501d38751ca0a694449ee53905f
“Toto” <elkourdi@localhost>
2023-01-15 18:11:58 +0100

    ajout de quelques commentaires explicatifs

--- src/Ford_Fulkerson.ml
+++ src/Ford_Fulkerson.ml
@@ -2,6 +2,8 @@ open Graph
 open Tools
 open Gfile
 
+
+
 let rec find_path gr s d acu1 li_out_arcs acu = 
   let acu = s::acu in
    match li_out_arcs with
@@ -14,6 +16,7 @@ let rec find_path gr s d acu1 li_out_arcs acu =
 
 let path gr s d = find_path gr s d [s] (out_arcs gr s) []
 
+(* fonction qui cherche le min dans un chemin donné*)
 let rec find_flow_min gr path min = match path with
   |[]->  min
   |[id] -> min
@@ -29,19 +32,20 @@ let rec print_id li_id = match li_id with
   |id :: tail -> Printf.printf "%d -> %!" id ;print_id tail
 
 
+  (* add_flow est une fonction qui rajoute une valeur v à tous les arcs d'un chemin donné *)
 let rec add_flow gr path v  = match path with 
   | [lbl] -> gr
   |id1::id2::tail ->
   add_flow  (add_arc (add_arc gr id1 id2 (-v)) id2 id1 v) (id2::tail) v
   |[] -> assert false
 
-
+(*flow_max est une fonction qui à chaque fois qu'elle trouve un chemin, elle calcule le min et le rajoute à ses arcs, jusqu'a ce qu'eklle ne trouve plus de chemin *)
 
 let flow_max gr s d  = 
   let rec aux path_found gr1 flow =
     print_id path_found;
     match path_found with 
-      |[]->"" flow
+      |[]->flow
       |chem ->  let min = find_min gr1 chem in 
       let graph_aux = add_flow gr1 chem min in
         aux (path graph_aux s d) graph_aux (flow + min)
